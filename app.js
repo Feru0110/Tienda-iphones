5c069cadf7f99c91659fd35e6bfc86c37da931c2
Feru0110 <mugalde70964@ufide.ac.cr>
2024-09-24 14:21:48 -0600

    update

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+
+
 document.querySelectorAll('.btn').forEach(button => {
     button.addEventListener('click', () => {
-        alert('Producto agregado al carrito');
+
+        const producto = button.parentNode.querySelector('h2').textContent;
+        const precio = button.parentNode.querySelector('.price').textContent;
+        alert(`Producto agregado al carrito: ${producto} - ${precio}`);
+
+
+        window.location.href = 'carrito.html';
     });
 });
