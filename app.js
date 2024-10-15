0a13b325d1459f4c63db9bea025a3e47e7ef2484
Feru0110 <mugalde70964@ufide.ac.cr>
2024-10-14 18:35:40 -0600

    update

--- app.js
+++ app.js
@@ -16,3 +16,42 @@ function agregarAlCarrito() {
 
     window.location.href = 'carrito.html';
 }
+
+
+const loginBtn = document.getElementById('login');
+const loginModal = document.getElementById('loginModal');
+const closeBtn = document.getElementById('closeBtn');
+const loginForm = document.getElementById('loginForm');
+
+
+loginBtn.addEventListener('click', () => {
+    loginModal.style.display = 'flex';
+});
+
+
+closeBtn.addEventListener('click', () => {
+    loginModal.style.display = 'none';
+});
+
+
+window.addEventListener('click', (e) => {
+    if (e.target === loginModal) {
+        loginModal.style.display = 'none';
+    }
+});
+
+
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault(); 
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+
+    if (email === 'admin@inova.cr' && password === '123456') {
+        alert('Inicio de sesión exitoso');
+        loginModal.style.display = 'none';
+    } else {
+        alert('Correo o contraseña incorrectos');
+    }
+});
